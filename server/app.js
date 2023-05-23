2f9a71aa51861239544f70d764f82d5a8a263beb
seongpil0948 <seongpil0948@gmail.com>
2023-05-23 15:19:07 +0900

    Feat(minisearch): add setup

--- server/app.js
+++ server/app.js
@@ -1,22 +1,75 @@
 // import fs from 'fs'
 // import https from 'https-localhost'
+import fs from 'fs'
 import jsonServer from 'json-server'
+import MiniSearch from 'minisearch'
 
 // 둘다 익스프레스인데.. 어떻게 못합치나..?
 // const httpsLocalhost = https()
-
-const router = jsonServer.router('server/db.json')
-// await httpsLocalhost.getCerts()
-
+const DB_PATH = 'server/db.json'
 const server = jsonServer.create({
   // key: fs.readFileSync('/home/sp/.config/https-localhost/localhost.key'),
   // cert: fs.readFileSync('/home/sp/.config/https-localhost/localhost.crt'),
 })
+const router = jsonServer.router(DB_PATH)
 const middleware = jsonServer.defaults()
-
+// await httpsLocalhost.getCerts()
+server.use(jsonServer.bodyParser)
 server.use(middleware)
+
+const ID_FIELD = 'id'
+const FIELD_LIST = [ID_FIELD, 'title', 'content']
+const postSearch = new MiniSearch({
+  fields: FIELD_LIST,
+  storeFields: FIELD_LIST,
+  idField: ID_FIELD,
+})
+
+// server.use((req, res, next) => {
+//   console.log('in middleware')
+//   if (req.method === 'POST')
+//     req.body.createdAt = Date.now()
+//   // Continue to JSON Server router
+//   next()
+// })
+
+// Add custom routes before JSON Server router
+server.get('/echo', (req, res) => {
+  res.jsonp(req.query)
+})
+
+server.get('/posts/search/:word', (req, res) => {
+  const buff = fs.readFileSync(DB_PATH)
+  const data = JSON.parse(buff)
+  postSearch.removeAll()
+  postSearch.addAll(data.posts)
+  console.log('search word: ', req.params.word)
+  res.jsonp(postSearch.search(req.params.word).reduce((acc, val) => {
+    const serialized = {}
+    for (let i = 0; i < FIELD_LIST.length; i++)
+      serialized[FIELD_LIST[i]] = val[FIELD_LIST[i]]
+
+    acc.push(serialized)
+    return acc
+  }, []))
+})
+server.get('/posts/search', (req, res) => {
+  const buff = fs.readFileSync(DB_PATH)
+  const data = JSON.parse(buff)
+  postSearch.removeAll()
+  postSearch.addAll(data.posts)
+  const word = req.query.word
+  console.info('search word: ', word)
+  const result = postSearch.autoSuggest(word)
+
+  res.jsonp(result.reduce((acc, val) => {
+    const keywords = val.suggestion.split(' ').map(x => x.trim())
+    return [...acc, ...keywords]
+  }, []))
+})
 server.use(router)
 // server.listen(443, () => {
 server.listen(3000, () => {
+  // console.log('JSON Server is running', server._router.stack)
   console.log('JSON Server is running')
 })
--- src/components/PostSearchSelector.vue
+++ src/components/PostSearchSelector.vue
@@ -11,12 +11,12 @@ console.info('pwaInfo in search', pwaInfo)
 const searchWord = ref('')
 const postList = ref<IPost[]>([])
 const router = useRouter()
-
+const BASE_URL = 'http://127.0.0.1:3000/posts'
 const handleSelect = (item: any) => {
   router.push(`/post/${encodeURIComponent(item.id)}`)
 }
 const searchPost = async (p?: { page?: number; size?: number }) => {
-  const url = `http://localhost:3000/posts?_page=${p?.page ?? 1}&_limit=${p?.size ?? 10}`
+  const url = `${BASE_URL}?_page=${p?.page ?? 1}&_limit=${p?.size ?? 10}`
   const res = await axios.get(url)
   postList.value = res.data
   emit('postList', postList.value)
@@ -69,18 +69,22 @@ const querySearchAsync = (queryString: string, cb: (arg: any) => void) => {
   console.log('queryString: ', queryString)
   if (!queryString)
     return cb([])
-  Promise.all([
-    axios.get(`http://localhost:3000/posts?title_like=${queryString}`),
-    axios.get(`http://localhost:3000/posts?content_like=${queryString}`),
-  ]).then((res) => {
-    const result: IPost[] = [...res[0].data, ...res[1].data].reduce((acc, val) => {
-      if (acc[val.id])
-        return acc
-      acc[val.id] = val
-      return acc
-    }, {} as { [k: string]: any })
-    cb(Object.values(result))
+  axios.get(`${BASE_URL}/search/${queryString}`).then((res) => {
+    cb(res.data)
   })
+
+  // Promise.all([
+  //   axios.get(`http://localhost:3000/posts?title_like=${queryString}`),
+  //   axios.get(`http://localhost:3000/posts?content_like=${queryString}`),
+  // ]).then((res) => {
+  //   const result: IPost[] = [...res[0].data, ...res[1].data].reduce((acc, val) => {
+  //     if (acc[val.id])
+  //       return acc
+  //     acc[val.id] = val
+  //     return acc
+  //   }, {} as { [k: string]: any })
+  //   cb(Object.values(result))
+  // })
 }
 </script>
 
